abad7c2273042995628baeed1de516b0eda3b7bd
Anthony Onorato <106277525+anthonyonorato@users.noreply.github.com>
2022-09-09 20:08:58 -0400

    Create UI variables

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Data Structures
+// Object Constructors
 
 class Book {
     constructor(
@@ -19,4 +19,39 @@ class Book {
       this.books = []
     }
   
-   
+    addBook(newBook) {
+        if (!this.isInLibrary(newBook)) {
+          this.books.push(newBook)
+        }
+      }
+    
+      removeBook(title) {
+        this.books = this.books.filter((book) => book.title !== title)
+      }
+    
+      getBook(title) {
+        return this.books.find((book) => book.title === title)
+      }
+    
+      isInLibrary(newBook) {
+        return this.books.some((book) => book.title === newBook.title)
+      }
+    }
+    
+    const library = new Library()
+    
+    // User Interface
+    
+    const accountBtn = document.getElementById('accountBtn')
+    const accountModal = document.getElementById('accountModal')
+    const addBookBtn = document.getElementById('addBookBtn')
+    const addBookModal = document.getElementById('addBookModal')
+    const errorMsg = document.getElementById('errorMsg')
+    const overlay = document.getElementById('overlay')
+    const addBookForm = document.getElementById('addBookForm')
+    const booksGrid = document.getElementById('booksGrid')
+    const loggedIn = document.getElementById('loggedIn')
+    const loggedOut = document.getElementById('loggedOut')
+    const loadingRing = document.getElementById('loadingRing')
+    
+ 
