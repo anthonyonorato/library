22af34b73054e1511121ccf1b2b8526391e2bc02
Anthony Onorato <106277525+anthonyonorato@users.noreply.github.com>
2022-09-10 01:33:12 -0400

    add appends

--- script.js
+++ script.js
@@ -138,3 +138,20 @@ class Book {
       author.textContent = book.author
       pages.textContent = `${book.pages} pages`
       removeBtn.textContent = 'Remove'
+
+      if (book.isRead) {
+        readBtn.textContent = 'Read'
+        readBtn.classList.add('btn-light-green')
+      } else {
+        readBtn.textContent = 'Not read'
+        readBtn.classList.add('btn-light-red')
+      }
+    
+      bookCard.appendChild(title)
+      bookCard.appendChild(author)
+      bookCard.appendChild(pages)
+      buttonGroup.appendChild(readBtn)
+      buttonGroup.appendChild(removeBtn)
+      bookCard.appendChild(buttonGroup)
+      booksGrid.appendChild(bookCard)
+    }
