4a670b42ae9f7befd78139be972d50ce8b60d371
Anthony Onorato <106277525+anthonyonorato@users.noreply.github.com>
2022-09-09 20:03:03 -0400

    create book object constructor

---
+++ script.js
@@ -0,0 +1,22 @@
+// Data Structures
+
+class Book {
+    constructor(
+      title = 'Unknown',
+      author = 'Unknown',
+      pages = '0',
+      isRead = false
+    ) {
+      this.title = title
+      this.author = author
+      this.pages = pages
+      this.isRead = isRead
+    }
+  }
+  
+  class Library {
+    constructor() {
+      this.books = []
+    }
+  
+   
