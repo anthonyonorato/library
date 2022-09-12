84aa5588740761748b29b254749fbe3a79d4f011
Anthony Onorato <106277525+anthonyonorato@users.noreply.github.com>
2022-09-12 13:28:43 -0400

    fix typo

--- script.js
+++ script.js
@@ -13,77 +13,77 @@ const booksDisplay = document.querySelector('#booksDisplay');
 // Store the "new" book objects into an array.
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault(); // prevents form from submitting
+    e.preventDefault(); // prevents form from submitting
 
-  let submittedTitle = e.target[0].value
-  let submittedAuthor = e.target[1].value
-  let submittedPages = e.target[2].value
-  let submittedIsRead = e.target[3].checked
+    let submittedTitle = e.target[0].value
+    let submittedAuthor = e.target[1].value
+    let submittedPages = e.target[2].value
+    let submittedIsRead = e.target[3].checked
 
-  let div = document.createElement("div");
-  div.setAttribute("class", "bookCard");
+    let div = document.createElement("div");
+    div.setAttribute("class", "bookCard");
 
-  let newTitle = document.createElement("p");
-  let newAuthor = document.createElement("p");
-  let newPages = document.createElement("p");
+    let newTitle = document.createElement("p");
+    let newAuthor = document.createElement("p");
+    let newPages = document.createElement("p");
 
-  let statusBtn = document.createElement("button");
-  let removeBtn = document.createElement("button");
- 
-  newTitle.innerText = submittedTitle;
-  newAuthor.innerText = submittedAuthor;
-  newPages.innerText = `${submittedPages} Pgs`;
-  statusBtn.setAttribute("id", "statusBtn");
-  removeBtn.setAttribute("class", "btn");
-  removeBtn.setAttribute("id", 'removeBtn')
-  removeBtn.innerText = "Remove";
+    let statusBtn = document.createElement("button");
+    let removeBtn = document.createElement("button");
 
-  if (submittedIsRead === true) {
-    statusBtn.setAttribute("class", "btn completedBtn");
-    statusBtn.innerText = "Completed";
-  } else if (submittedIsRead === false) {
-    statusBtn.setAttribute("class", "btn notCompletedBtn");
-    statusBtn.innerText = "Not Completed";
-  } else {
-      alert("Invalid Entry")
-  }
+    newTitle.innerText = submittedTitle;
+    newAuthor.innerText = submittedAuthor;
+    newPages.innerText = `${submittedPages} Pgs`;
+    statusBtn.setAttribute("id", "statusBtn");
+    removeBtn.setAttribute("class", "btn");
+    removeBtn.setAttribute("id", 'removeBtn')
+    removeBtn.innerText = "Remove";
 
-  statusBtn.addEventListener('click', (btn)=>{
-    let btnText = btn.target.innerText
-    if(btnText === "Completed"){
-        btn.target.className = "btn notCompletedBtn";
-        btn.target.innerText = "Not Completed";
-        return
+    if (submittedIsRead === true) {
+      statusBtn.setAttribute("class", "btn completedBtn");
+      statusBtn.innerText = "Completed";
+    } else if (submittedIsRead === false) {
+      statusBtn.setAttribute("class", "btn notCompletedBtn");
+      statusBtn.innerText = "Not Completed";
+    } else {
+        alert("Invalid Entry")
     }
-    if(btnText === "Not Completed"){
-        btn.target.className = "btn completedBtn";
-        btn.target.innerText = "Completed";
-        return
-    }
-})
 
-removeBtn.addEventListener('click', (e) =>{
-    e.path[1].remove()
-})
+    statusBtn.addEventListener('click', (btn)=>{
+        let btnText = btn.target.innerText
+        if(btnText === "Completed"){
+            btn.target.className = "btn notCompletedBtn";
+            btn.target.innerText = "Not Completed";
+            return
+        }
+        if(btnText === "Not Completed"){
+            btn.target.className = "btn completedBtn";
+            btn.target.innerText = "Completed";
+            return
+        }
+    })
 
-div.appendChild(newTitle);
-div.appendChild(newAuthor);
-div.appendChild(newPages);
-div.appendChild(statusBtn);
-div.appendChild(removeBtn);
+    removeBtn.addEventListener('click', (e) =>{
+        e.path[1].remove()
+    })
 
-booksDisplay.appendChild(div);
+    div.appendChild(newTitle);
+    div.appendChild(newAuthor);
+    div.appendChild(newPages);
+    div.appendChild(statusBtn);
+    div.appendChild(removeBtn);
 
-overlay.classList.remove('active');
-formContainer.classList.remove('active');
+    booksDisplay.appendChild(div);
+
+    overlay.classList.remove('active');
+    formContainer.classList.remove('active');
 })
 
 addBookBtn.addEventListener('click', ()=>{
-overlay.classList.add('active')
-formContainer.classList.add('active')
+    overlay.classList.add('active')
+    formContainer.classList.add('active')
 })
 
 overlay.addEventListener('click', ()=>{
-overlay.classList.remove('active')
-formContainer.classList.remove('active')
+    overlay.classList.remove('active')
+    formContainer.classList.remove('active')
 })
