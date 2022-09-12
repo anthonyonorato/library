8b1adc45ace7d11da796e83d9de57b6d376062b5
Anthony Onorato <106277525+anthonyonorato@users.noreply.github.com>
2022-09-12 12:12:52 -0400

    add eventlistener for adding and "storing" new book

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ const overlay = document.querySelector('.overlay');
 const formContainer = document.querySelector('.formContainer');
 
 const booksDisplay = document.querySelector('#booksDisplay');
+
+// Store the "new" book objects into an array.
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); // prevents form from submitting
+
+  let submittedTitle = e.target[0].value
+  let submittedAuthor = e.target[1].value
+  let submittedPages = e.target[2].value
+  let submittedIsRead = e.target[3].checked
+
